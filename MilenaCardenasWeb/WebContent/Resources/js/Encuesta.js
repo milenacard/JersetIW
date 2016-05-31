f26917f3df5bba1d310b7d75f0f2c997c2d5d357
Milena Card <Milena Card>
2016-05-30 19:18:14 -0500

    crear encuesta

---
+++ MilenaCardenasWeb/WebContent/Resources/js/Encuesta.js
@@ -0,0 +1,47 @@
+var mod = angular.module('Encuesta', []);
+
+mod.controller('preguntasEncuesta', ['$scope', function($scope) {
+	
+	$scope.preguntas=[{id:1,
+		texto: 'pregunta 1',
+		respuestaValida: 1,
+		respuesta: null,
+		estado: '',
+		respuestas:[{id:1,
+	                texto: 'opcion 1'},
+	                {id:2,
+	                texto: 'opcion 2'},
+	                {id:3,
+	                texto: 'opcion 3'}]	
+	
+	}, {id: 2,
+		texto: 'pregunta2',
+		respuestaValida: 1,
+		respuesta:null,
+		estado: '',
+		respuesta:[{id:1,
+            texto: 'opcion 1'},
+            {id:2,
+            texto: 'opcion 2'},
+            {id:3,
+            texto: 'opcion 3'}]		
+	}
+	];
+	$scope.respuestaValidar = 0;
+	
+	//Valida que la respuesta ingresada sea valida
+	$scope.validar = function(pregunta){
+		if (pregunta.respuesta == pregunta.respuestaValida) {
+			$scope.respuestasValidar++; //mirar bien
+			pregunta.estado = 'ok';
+		}else{
+			if (pregunta.estado=='ok'&& $scope.respuestaValidar>0) {
+				$scope.respuestaValidar--;
+			}
+			pregunta.estado='error';
+		}
+	}
+	
+}]);
+	
+	
